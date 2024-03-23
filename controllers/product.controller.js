98ea4af03109757ccd73d0b587534b0c1ecc7aee
K. Aslanoglou <k_aslanoglou@hotmail.com>
2024-03-23 21:00:01 +0200

    logger product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product.model')
+const logger = require('../logger/logger')
 
 exports.findAll = async(req, res) => {
     console.log('Find all products')
@@ -6,8 +7,11 @@ exports.findAll = async(req, res) => {
     try{
         const result = await Product.find()
         res.status(200).json({data:result})
+        logger.debug("Success in reading all products")
+        logger.info("Success in reading all products")
     } catch(err) {
         console.log(`Problem in reading products, ${err}`)
+        logger.error(`Problem in reading all users, ${err}`)
     }
 }
 
@@ -19,9 +23,11 @@ exports.findOne = async(req, res) => {
     try{
     const result = await Product.findOne({_id:_id})
     res.status(200).json({data:result})
+    logger.info("Success in reading product")
     }
     catch(err) {
         console.log(`Problem in reading product, ${err}`)
+        logger.info(`Problem in reading product, ${err}`)
     }
 }
 
@@ -40,10 +46,13 @@ exports.create = async(req, res) => {
     try{
         const result = await newProduct.save()
         res.status(200).json({data:result})
+        logger.info("Success in saving product")
         console.log('Product saved')
     } catch(err) {
         res.status(400).json({data:err})
         console.log("Problem in saving product")
+        logger.error("Problem in saving product")
+
     }
 }
 
@@ -65,11 +74,12 @@ exports.update = async(req, res) => {
             {new:true}
         )
         res.status(200).json({data:result})
+        logger.info("Success in updating product with id: ", _id)
         console.log('Success in updating product with id: ', _id)
     } catch(err) {
         res.status(400).json({data:err})
+        logger.info("Problem in updating product with id: ", _id)
         console.log('Problem in updating product with id: ', _id)
-
     }
 }
 
@@ -82,9 +92,12 @@ exports.delete = async(req, res) => {
             _id:_id
         })
         res.status(200).json({data:result})
+        logger.info("Success in deleting product ", _id)
+
         console.log('Success in deleting product: ', _id)
     } catch(err) {
         res.status(400).json({data:err})
+        logger.error("Problem in deleting product ", _id)
         console.log('Problem in deleting product: ', _id)
     }
 }
